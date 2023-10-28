433b37077c0928e4819b715ecfea7f4e6e62bb7a
darq <prototypid@gmail.com>
2023-10-28 08:29:42 +0530

    Add mongoose and function to connect to database

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8800;
+const user = process.env.MONGO_USERNAME || "admin";
+const pwd = process.env.MONGO_PASSWORD;
+const m_port = process.env.MONGO_PORT || "localhost";
+const m_host = process.env.MONGO_HOST || 27017;
+const m_db = process.env.MONGO_DB || "fiverr";
+
+const connect = async function () {
+  try {
+    await mongoose.connect(
+      `mongodb://${user}:${pwd}@${m_host}:${m_port}/${m_db}?authSource=admin`
+    );
+    console.log("Connected to Mongodb");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 
 app.listen(port, () => {
+  connect();
   console.log("Backend Server is running!");
 });
