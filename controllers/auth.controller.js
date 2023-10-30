df726680cea16e74aafef2f4be1b27057da99ad1
darq <prototypid@gmail.com>
2023-10-30 12:33:41 +0530

    Add login controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -17,6 +17,20 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return res.status(404).send("User not found!");
+
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+    if (!isCorrect) return res.status(400).send("Wrong password or username!");
+
+    const { password, ...info } = user._doc;
+    return res.status(200).send(info);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Something went wrong!");
+  }
+};
 
 export const logout = async (req, res) => {};
