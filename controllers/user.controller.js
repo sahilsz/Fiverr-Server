19a6507373a90059890962d93513a055798c9020
darq <prototypid@gmail.com>
2023-10-31 13:32:10 +0530

    Update user delete route to use jwt middleware

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,16 +5,13 @@ export const deleteUser = async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
 
-    const token = req.cookies.accessToken;
-    if (!token) return res.status(401).send("You are not authenticated!");
+    if (req.useId !== user._id.toString()) {
+      return res.status(403).send("You can only delete your account!");
+    }
 
-    jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
-      if (payload.id !== user._id.toString()) {
-        return res.status(403).send("You can only delete your account!");
-      }
-      await User.findByIdAndDelete(req.params.id);
-      return res.status(200).send("Deleted!");
-    });
+    await User.findByIdAndDelete(req.params.id);
+
+    return res.status(200).send("Deleted!");
   } catch (err) {
     console.log(err);
   }
