c296b28efa2342fb15a8e50596586b70042250e2
darq <prototypid@gmail.com>
2023-11-06 17:23:34 +0530

    feat: Add route to create new chat

--- controllers/conversation.controller.js
+++ controllers/conversation.controller.js
@@ -13,3 +13,22 @@ export const getConversationsHandler = async (req, res, next) => {
   }
 };
 
+export const createConversationHandler = async (req, res, next) => {
+  const newConversation = new Conversation({
+    // sellerId needs to be the first
+    id: req.isSeller ? req.userId + req.body.to : req.body.to + req.userid,
+    sellerId: req.isSeller ? req.userId : req.body.to,
+    buyerId: req.isSeller ? req.body.to : req.userId,
+    readBySeller: req.isSeller,
+    readByBuyer: !req.isSeller,
+  });
+
+  try {
+    const conv = await newConversation.save();
+
+    return res.status(201).send(conv);
+  } catch (err) {
+    next(err);
+  }
+};
+
--- routes/conversation.route.js
+++ routes/conversation.route.js
@@ -2,9 +2,12 @@ import express from "express";
 import { verifyToken } from "../middleware/jwt.js";
 import {
   getConversationHandler,
+  createConversationHandler,
+} from "../controllers/conversation.controller.js";
 
 const router = express.Router();
 
 router.get("/", verifyToken, getConversationsHandler);
+router.post("/", verifyToken, createConversationHandler);
 
 export default router;
